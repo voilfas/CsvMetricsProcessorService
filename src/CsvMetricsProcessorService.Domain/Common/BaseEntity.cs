615e2cc0f4b52241f505ff1df8c63ba42a079368
Artyom <sobkalovt@bk.ru>
2026-08-05 13:31:37 +0300

    chore: add BaseEntity for domain entities

---
+++ src/CsvMetricsProcessorService.Domain/Common/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿namespace CsvMetricsProcessorService.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not BaseEntity entity)
+            return false;
+        
+        return Id == entity.Id;
+    }
+    
+    public override int GetHashCode() => Id.GetHashCode();
+}
