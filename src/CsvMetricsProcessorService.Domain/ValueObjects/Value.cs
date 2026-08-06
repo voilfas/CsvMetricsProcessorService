2bdaabd6a6d5b27610105cc39959e4909305f132
Artyom <sobkalovt@bk.ru>
2026-08-06 13:35:38 +0300

    feat: add value object Value

---
+++ src/CsvMetricsProcessorService.Domain/ValueObjects/Value.cs
@@ -0,0 +1,27 @@
+﻿using CsvMetricsProcessorService.Domain.Errors;
+using CsvMetricsProcessorService.Domain.Results;
+
+namespace CsvMetricsProcessorService.Domain.ValueObjects;
+
+public record Value
+{
+    public double Indicator { get; private set; }
+
+    private Value(double indicator)
+    {
+        Indicator = indicator;
+    }
+
+    public static Result<Value> Create(double indicator)
+    {
+        if (double.IsInfinity(indicator) || double.IsNaN(indicator))
+            return Result<Value>.Failure(
+                DomainErrors.Value.InfinityNan);
+
+        if (indicator < 0)
+            return Result<Value>.Failure(
+                DomainErrors.Value.Negative);
+
+        return Result<Value>.Success(new Value(indicator));
+    }
+}
