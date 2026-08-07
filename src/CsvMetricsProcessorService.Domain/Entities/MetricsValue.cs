761ad8bf607374200c639d4b279ec6c84148ca4e
Artyom <sobkalovt@bk.ru>
2026-08-07 18:52:37 +0300

    refactor: update MetricsValue from DateStart to Date

--- src/CsvMetricsProcessorService.Domain/Entities/MetricsValue.cs
+++ src/CsvMetricsProcessorService.Domain/Entities/MetricsValue.cs
@@ -9,7 +9,7 @@ public class MetricsValue : BaseEntity
 {
     private static readonly DateTime MinDate = new DateTime(2000, 1, 1);
     public FileName FileName { get; private set; }
-    public DateTime DateStart { get; private set; }
+    public DateTime Date { get; private set; }
     public ExecutionTime ExecutionTime { get; private set; }
     public Value Value { get; private set; }
 
@@ -19,12 +19,12 @@ public class MetricsValue : BaseEntity
 
     private MetricsValue(
         FileName fileName,
-        DateTime dateStart,
+        DateTime date,
         ExecutionTime executionTime,
         Value value)
     {
         FileName = fileName;
-        DateStart = dateStart;
+        Date = date;
         ExecutionTime = executionTime;
         Value = value;
     }
