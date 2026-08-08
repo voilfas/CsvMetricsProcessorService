0dc8ca00d5718b24cedccc1bf992ef44357cb4d2
Artyom <sobkalovt@bk.ru>
2026-08-08 21:26:01 +0300

    feat: add DTOs for interfaces

---
+++ src/CsvMetricsProcessorService.Application/DTOs/MetricsValueDto.cs
@@ -0,0 +1,12 @@
+﻿using CsvMetricsProcessorService.Domain.ValueObjects;
+
+namespace CsvMetricsProcessorService.Application.DTOs;
+
+public record MetricsValueDto(
+    Guid Id,
+    string FileName,
+    DateTime Date,
+    double ExecutionTime,
+    double Value,
+    DateTime CreatedAt
+    );
