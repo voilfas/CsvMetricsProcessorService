f11a562b88210089dc1aa616f3b309539dbd317b
Artyom <sobkalovt@bk.ru>
2026-08-08 21:25:24 +0300

    feat: add interfaces for all methods

---
+++ src/CsvMetricsProcessorService.Application/Interfaces/ICsvParser.cs
@@ -0,0 +1,10 @@
+﻿using CsvMetricsProcessorService.Domain.Entities;
+using CsvMetricsProcessorService.Domain.Results;
+using CsvMetricsProcessorService.Domain.ValueObjects;
+
+namespace CsvMetricsProcessorService.Application.Interfaces;
+
+public interface ICsvParser
+{
+    Task<Result<IReadOnlyList<MetricsValue>>> ParseAsync(Stream csvStream, FileName fileName, CancellationToken ct);
+}
---
+++ src/CsvMetricsProcessorService.Application/Interfaces/IMetricsRepository.cs
@@ -0,0 +1,16 @@
+﻿using CsvMetricsProcessorService.Domain.Entities;
+using CsvMetricsProcessorService.Domain.Results;
+using CsvMetricsProcessorService.Domain.ValueObjects;
+
+namespace CsvMetricsProcessorService.Application.Interfaces;
+
+public interface IMetricsRepository
+{
+    Task<bool> ExistsAsync(FileName fileName, CancellationToken ct);
+    
+    Task DeleteByFileNameAsync(FileName fileName, CancellationToken ct);
+    
+    Task AddMetricsValueAsync(List<MetricsValue> metricsValue, CancellationToken ct);
+    
+    void AddMetricsResult(MetricsResult metricsResult);
+}
