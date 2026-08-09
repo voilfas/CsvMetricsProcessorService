316d1f8e8cf6fadbc0f51310b3eff0ed24d9512d
Artyom <sobkalovt@bk.ru>
2026-08-09 14:50:53 +0300

    feat: add configurations

---
+++ src/CsvMetricsProcessorService.Infrastructure/Persistence/Configurations/MetricsResultConfiguration.cs
@@ -0,0 +1,66 @@
+﻿using CsvMetricsProcessorService.Domain.Entities;
+using CsvMetricsProcessorService.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CsvMetricsProcessorService.Infrastructure.Persistence.Configurations;
+
+public class MetricsResultConfiguration : IEntityTypeConfiguration<MetricsResult>
+{
+    public void Configure(EntityTypeBuilder<MetricsResult> builder)
+    {
+        builder.ToTable("metrics_results");
+        
+        builder.HasKey(r => r.Id);
+        
+        builder.Property(r => r.Id)
+            .HasColumnName("id")
+            .ValueGeneratedNever()
+            .IsRequired();
+        
+        builder.Property(r => r.FileName)
+            .HasConversion(
+                fullName => fullName!.Value,
+                value => FileName.FromDb(value))
+            .HasColumnName("file_name")
+            .HasMaxLength(255)
+            .IsRequired();
+        
+        builder.Property(r => r.DeltaDate)
+            .HasColumnName("delta_date")
+            .IsRequired();
+        
+        builder.Property(r => r.MinDate)
+            .HasColumnName("min_date")
+            .IsRequired();
+        
+        builder.Property(r => r.AvgExecutionTime)
+            .HasColumnName("avg_execution_time")
+            .IsRequired();
+        
+        builder.Property(r => r.AvgValue)
+            .HasColumnName("avg_value")
+            .IsRequired();
+        
+        builder.Property(r => r.MedianValue)
+            .HasColumnName("median_value")
+            .IsRequired();
+        
+        builder.Property(r => r.MaxValue)
+            .HasColumnName("max_value")
+            .IsRequired();
+        
+        builder.Property(r => r.MinValue)
+            .HasColumnName("min_value")
+            .IsRequired();
+        
+        builder.Property(r => r.CreatedAt)
+            .HasColumnName("created_at")
+            .IsRequired();
+        
+        builder.HasIndex(r => r.FileName);
+        builder.HasIndex(r => r.MinDate);
+        builder.HasIndex(r => r.AvgValue);
+        builder.HasIndex(r => r.AvgExecutionTime);
+    }
+}
---
+++ src/CsvMetricsProcessorService.Infrastructure/Persistence/Configurations/MetricsValueConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using CsvMetricsProcessorService.Domain.Entities;
+using CsvMetricsProcessorService.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CsvMetricsProcessorService.Infrastructure.Persistence.Configurations;
+
+public class MetricsValueConfiguration : IEntityTypeConfiguration<MetricsValue>
+{
+    public void Configure(EntityTypeBuilder<MetricsValue> builder)
+    {
+        builder.ToTable("metrics_values");
+
+        builder.HasKey(v => v.Id);
+
+        builder.Property(v => v.Id)
+            .HasColumnName("id")
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(v => v.FileName)
+            .HasConversion(
+                fullName => fullName.Value,
+                value => FileName.FromDb(value))
+            .HasColumnName("file_name")
+            .HasMaxLength(255)
+            .IsRequired();
+        
+        builder.Property(v => v.Date)
+            .HasColumnName("date")
+            .IsRequired();
+        
+        builder.Property(v => v.ExecutionTime)
+            .HasConversion(
+                executionTime => executionTime.TotalSeconds,
+                value => ExecutionTime.Create(value).Value!)
+            .HasColumnName("execution_time")
+            .IsRequired();
+        
+        builder.Property(v => v.Value)
+            .HasConversion(
+                indicator => indicator.Indicator,
+                value => Value.FromDb(value))
+            .HasColumnName("value")
+            .IsRequired();
+
+        builder.Property(v => v.CreatedAt)
+            .HasColumnName("created_at")
+            .IsRequired();
+
+        builder.HasIndex(v => new { v.FileName, v.Date }).IsDescending(false, true);
+
+    }
+}
